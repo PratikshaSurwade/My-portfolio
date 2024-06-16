e772e6219194db06a291d59858999eee331a689b
PratikshaSurwade <89350917+PratikshaSurwade@users.noreply.github.com>
2024-06-16 23:00:18 +0530

    Theme Change

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ window.onscroll = function() {
     scroll()
 };
 
-function scroll() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("navTabs").style.top = "0";
-    }
-    else {
-        document.getElementById("navTabs").style.top = "-60px";
-    }
-}
+// function scroll() {
+//     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//         document.getElementById("navTabs").style.top = "0";
+//     }
+//     else {
+//         document.getElementById("navTabs").style.top = "-50px";
+//     }
+// }
