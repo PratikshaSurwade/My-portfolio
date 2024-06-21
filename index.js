69e7e3de3bb4280b8bda7594cf35f6b695f41950
PratikshaSurwade <89350917+PratikshaSurwade@users.noreply.github.com>
2024-06-21 22:14:26 +0530

    Size Changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const elements = document.querySelectorAll('.word');
-
+console.log(elements);
     window.addEventListener('scroll', function() {
         elements.forEach((element, index) => {
             const rect = element.getBoundingClientRect();
