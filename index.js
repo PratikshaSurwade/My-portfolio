7842bd8e1a882113f70516f59f23260519bf46e7
PratikshaSurwade <89350917+PratikshaSurwade@users.noreply.github.com>
2024-08-28 22:19:18 +0530

    Design Changes

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+// Cursor animation
+
 var crsr = document.querySelector("#cursor");
 
 document.addEventListener("mousemove",function(details){
@@ -23,6 +25,7 @@ document.addEventListener("",function(details){
     crsr.style.left = details.x+"px";
     crsr.style.top = details.y+"px";
 })
+
 
 // Navbar Animation
 
@@ -41,6 +44,26 @@ gsap.from(".navTabs .btn",{
     stagger:0.3
 })
 
+//Heading Name Animation
+
+gsap.utils.toArray(".titleName").forEach((heading) => {
+    gsap.from(heading.querySelectorAll(".animateTitleWords"), {
+        y: 20,
+        opacity: 0,
+        stagger: 0.5,
+        // duration: 2,
+        ease: "power3.out",
+        scrollTrigger: {
+            trigger: heading,
+            scroller: "body",
+            markers:true,
+            start: "top 80%",  // Animation starts when the element hits 80% from the top of the viewport
+            end: "top 50%",    // Animation ends when the element is at 50% from the top of the viewport
+            toggleActions: "play none none none"  // The animation will only play once when the element comes into view
+        }
+    });
+});
+ 
 var tl= gsap.timeline();
 
 tl.from("dividerLine",{
@@ -50,8 +73,37 @@ tl.from("dividerLine",{
     delay:0.1,
 })
 
+
 // var loader = document.querySelector(".loader")
 
 // setTimeout(function () {
 //     loader.style.top = "-100%"
 // }, 4000)
+
+
+
+// Select all h1 elements with the class "animate-text"
+const introTexts = document.querySelectorAll('.animate-text');
+
+introTexts.forEach((introText) => {
+    const text = introText.textContent;
+    introText.innerHTML = '';
+
+    // Loop through each letter and wrap it in a span with a delay
+    text.split('').forEach((letter, index) => {
+        const span = document.createElement('span');
+        span.textContent = letter;
+        span.classList.add('letter');
+
+        // Apply animation on hover with delay based on index
+        span.addEventListener('mouseenter', () => {
+            span.classList.add('animate__animated', 'animate__rubberBand');
+            span.addEventListener('animationend', () => {
+                span.classList.remove('animate__animated', 'animate__rubberBand');
+            }, { once: true });
+        });
+
+        introText.appendChild(span);
+    });
+});
+
